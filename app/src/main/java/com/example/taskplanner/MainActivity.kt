2fa9fe6912759b45714ab87ecb6eb4b3c1dc2bb0
Lihini Ullandupitiya <lihiniullandupitiya@gmail.com>
2024-05-11 17:05:43 +0530

    Created the fundamental components

--- app/src/main/java/com/example/taskplanner/MainActivity.kt
+++ app/src/main/java/com/example/taskplanner/MainActivity.kt
@@ -1,11 +1,44 @@
 package com.example.taskplanner
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import com.example.taskplanner.databinding.ActivityMainBinding
+import com.example.taskplanner.utils.setupDialog
 
 class MainActivity : AppCompatActivity() {
+
+    private val mainBinding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private val addTaskDialog: Dialog by lazy {
+        Dialog(this, R.style.DialogCustomTheme).apply {
+            setupDialog(R.layout.add_task_dialog)
+        }
+    }
+
+    private val updateTaskDialog: Dialog by lazy {
+        Dialog(this, R.style.DialogCustomTheme).apply {
+            setupDialog(R.layout.update_task_dialog)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(mainBinding.root)
+
+        // Add task start
+        val addCloseImg = addTaskDialog.findViewById<ImageView>(R.id.closeImg)
+        val updateCloseImg = updateTaskDialog.findViewById<ImageView>(R.id.closeImg)
+
+        addCloseImg.setOnClickListener { addTaskDialog.dismiss() }
+        updateCloseImg.setOnClickListener { updateTaskDialog.dismiss() }
+
+        mainBinding.addTaskFABtn.setOnClickListener {
+            addTaskDialog.show()
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/taskplanner/utils/util.kt
@@ -0,0 +1,19 @@
+package com.example.taskplanner.utils
+
+import android.app.Dialog
+import android.content.Context
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.Toast
+import com.google.android.material.textfield.TextInputLayout
+
+fun Dialog.setupDialog(layoutResId: Int){
+    setContentView(layoutResId)
+    window!!.setLayout(
+        LinearLayout.LayoutParams.MATCH_PARENT,
+        LinearLayout.LayoutParams.WRAP_CONTENT
+    )
+    setCancelable(false)
+}
