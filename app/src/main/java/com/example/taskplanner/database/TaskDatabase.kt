3dfa216ecb4a1601314bda1c4c80bc2f7a2f6fce
Lihini Ullandupitiya <lihiniullandupitiya@gmail.com>
2024-05-11 20:24:02 +0530

    Integration of Room database

---
+++ app/src/main/java/com/example/taskplanner/database/TaskDatabase.kt
@@ -0,0 +1,35 @@
+package com.example.taskplanner.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.example.taskplanner.converters.TypeConverter
+import com.example.taskplanner.models.Task
+
+@Database(
+    entities = [Task::class],
+    version = 1,
+    exportSchema = false
+)
+@TypeConverters(TypeConverter::class)
+abstract class TaskDatabase : RoomDatabase() {
+
+    companion object {
+        @Volatile
+        private var INSTANCE: TaskDatabase? = null
+        fun getInstance(context: Context): TaskDatabase {
+            synchronized(this) {
+                return INSTANCE ?: Room.databaseBuilder(
+                    context.applicationContext,
+                    TaskDatabase::class.java,
+                    "task_db"
+                ).build().also {
+                    INSTANCE = it
+                }
+            }
+
+        }
+    }
+}
